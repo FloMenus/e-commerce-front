d01d29ff545bb246d83bfd02c13ec75fa3e3e51a
Danh-Le <quang.danh.le@gmail.com>
2022-09-08 13:33:49 +0200

    title change

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,13 +1,17 @@
-import Option from "./Option"
+import Option from "./Option";
 
 const Select = ({ handleChange, value, options }) => {
   return (
     <select onChange={handleChange} value={value}>
       {options.map((option) => (
-        <Option key={option.value} value={option.value} content={option.content} />
+        <Option
+          key={option.value}
+          value={option.value}
+          content={option.content}
+        />
       ))}
     </select>
-  )
-}
+  );
+};
 
-export default Select
+export default Select;
